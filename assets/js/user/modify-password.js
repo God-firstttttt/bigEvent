6a204e5c1b3360958fad717b06b9d1f5f5c277ed
Godfirst <472812931@qq.com>
2021-01-29 19:41:25 +0800

    新增修改密码模块

---
+++ assets/js/user/modify-password.js
@@ -0,0 +1,44 @@
+$(function() {
+
+    const { form, layer } = layui;
+
+    // 表单校验
+    form.verify({
+        pass: [/^\w{6,12}/,
+            '密码必须6·12位，切不能出现空格'
+        ],
+        confirmPass: function(val) {
+            if (val !== $('#pass').val()) { //val当前表单值
+                return '两次密码输入不同';
+            }
+        }
+    });
+
+    // 提交表单
+    $('.layui-form').submit(function(e) {
+        e.preventDefault();
+
+        // 发起ajax请求
+        axios.post('/my/updatepwd', $(this).serialize())
+            .then(res => {
+                // 检验请求失败
+                if (res.status !== 0) {
+                    return layer.msg('密码修改失败！')
+                }
+                // 检验请求成功
+                layer.msg('密码修改成功！')
+                $(this)[0].reset();
+            })
+    })
+
+
+
+
+
+
+
+
+
+
+
+})
