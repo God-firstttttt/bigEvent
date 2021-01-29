955d04fc4e5069f4b11faacb7cb6116462418a89
Godfirst <472812931@qq.com>
2021-01-29 15:36:19 +0800

    完成了登录之后首页功能

--- assets/js/commonAPI.js
+++ assets/js/commonAPI.js
@@ -4,16 +4,41 @@ axios.defaults.baseURL = 'http://api-breakingnews-web.itheima.net';
 // 添加全局的请求拦截器
 axios.interceptors.request.use(function(config) {
     console.log('-----发送ajax请求前');
-    // 在发送请求之前做些什么
+
+    console.log(config.url);
+    // 获取本地存储的token令牌
+    const token = localStorage.getItem('token') || '';
+
+    // 在发送请求之前判断是否有/my的请求路径
+    //如果有就添加headers
+    /*
+    正则表达式方法：\^/my\.test
+    startsWith('/my')
+    indexOf('/my')==0
+    */
+    if (config.url.startsWith('/my')) {
+        config.headers.Authorization = token;
+    }
     return config;
 }, function(error) {
     // 对请求错误做些什么
     return Promise.reject(error);
 });
 
+
 // 添加响应拦截器
 axios.interceptors.response.use(function(response) {
-    console.log('-----接收ajax响应前');
+    console.log('-----接收ajax响应前', response);
+    // 解构出需要的值
+    const { message, status } = response.data;
+    // 先判断身份验证是否失败
+
+    if (message == '身份认证失败！' && status == 1) {
+        // 清除本地存储的token
+        localStorage.removeItem('token');
+        // 跳转到登录页
+        location.href = './login.html';
+    }
     // 对响应数据做点什么
     return response.data;
 }, function(error) {
---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+$(function() {
+    // 从layui中提取模块
+    const { layer } = layui;
+
+    // 获取用户个人信息
+    function getUserInfo() {
+
+        axios.get('/my/userinfo').then(res => {
+            console.log(res);
+            // 先判断获取信息是否失败
+            if (res.status !== 0) {
+                return layer.msg('获取用户信息失败！');
+            }
+            // 从res中解析出data
+            const { data } = res;
+            // 将昵称或用户名赋值给name，如果昵称为空就用用户名
+            const name = data.nikname || data.username;
+            // 渲染昵称
+            $('#welcome').text(`欢迎 ${name}`);
+            // 渲染头像
+            if (data.user_pic) {
+                // 注意：base64格式的编码字符串可以直接设置给src
+                $('.avatar').prop('src', data.user_pic).show();
+                $('.text-avatar').hide();
+            } else {
+                $('.text-avatar').text(name[0].toUpperCase()).show();
+                $('.avatar').hide();
+            }
+        })
+    };
+
+    getUserInfo();
+
+    // 点击退出
+    $('#logout').click(function() {
+        // 请求接口（模拟）
+        // 清除本地token令牌
+        localStorage.removeItem('token');
+        // 跳转到登录页
+        location.href = './login.html';
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
